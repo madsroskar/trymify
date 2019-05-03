9be8a1102223257481580cac511ac356e9d81839
Mads Røskar <madshvero@gmail.com>
2019-05-03 10:15:10 +0200

    Add support for roasted sweet potato

--- String.prototype.trym.js
+++ String.prototype.trym.js
@@ -1,5 +1,15 @@
 if (!String.prototype.trym) {
-    String.prototype.trym = function() {
-       return this.replace(new RegExp(".", "gm"), "🥔")
+
+    function getPotato(type) {
+        switch(type) {
+            case "roasted":
+                return "🍠";
+            default:
+                return "🥔";
+        }
+    }
+
+    String.prototype.trym = function(type = "normal") {
+       return this.replace(new RegExp(".", "gm"), getPotato(type))
     }
 }
